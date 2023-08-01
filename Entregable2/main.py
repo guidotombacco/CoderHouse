74d95d630772592efde9720134cffb8330d8367f
guidotombacco <70452279+guidotombacco@users.noreply.github.com>
2023-08-01 15:46:37 -0300

    Update main.py

--- Entregable2/main.py
+++ Entregable2/main.py
@@ -1,8 +1,15 @@
 import requests
 from collections import namedtuple
-import json
-from json import JSONEncoder
 import redshift_connector
+
+import pandas as pd
+from sqlalchemy import create_engine, text
+import psycopg2
+hostname= 'data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com'
+database= 'data-engineer-database'
+username= 'guidotombacco_coderhouse'
+pwd='Jd2iE008KW'
+port_id= '5439'
 
 r = requests.get("https://api.open-meteo.com/v1/forecast?latitude=-29.7125&longitude=-57.0877&hourly=temperature_2m,precipitation_probability,rain,visibility,windspeed_10m&timezone=America%2FSao_Paulo&forecast_days=1")
 
@@ -10,18 +17,10 @@ r = requests.get("https://api.open-meteo.com/v1/forecast?latitude=-29.7125&longi
 def customClimaDecoder(climaDict):
     return namedtuple('X', climaDict.keys())(*climaDict.values())
 
-
-json_object = json.loads(r.text, object_hook=customClimaDecoder)
-
-tiempo = json_object.hourly.time
-temperatura = json_object.hourly.temperature_2m
-precipitacion = json_object.hourly.precipitation_probability
-lluvia = json_object.hourly.rain
-visibilidad = json_object.hourly.visibility
-viento_velocidad = json_object.hourly.windspeed_10m
+df = pd.read_json(r.text)
 
 redshift_connector.paramstyle = 'numeric'
-
+engine= create_engine("postgresql://postgres:david9.25.38@localhost:5432/Semana6_DE")
 conn = redshift_connector.connect(
     host='data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com',
     database='data-engineer-database',
@@ -32,6 +31,8 @@ conn = redshift_connector.connect(
 
 conn.autocommit = True
 cursor = conn.cursor()
+
+df.to_sql()
 
 for i in range(len(tiempo)):
     sql = 'insert into clima(fecha, temperatura, precipitacion, lluvia, visibilidad, velocidadViento) VALUES(:1, :2, :3, :4, :5, :6)'
